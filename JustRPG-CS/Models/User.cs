3a55b18e388a03309d14fd57fedf13f41e32d314
noname <smershicus@gmail.com>
2022-10-28 21:58:57 +0300

    add class for more comfortable get and using user equipment

--- JustRPG-CS/Models/User.cs
+++ JustRPG-CS/Models/User.cs
@@ -1,7 +1,10 @@
+using Serilog;
+
 namespace JustRPG_CS.Classes;
 
-public record User
+public class User
 {
+
     public long id { get; set; } = 0;
     public int cash { get; set; }= 0;
     public string cooldown { get; set; } = "";
@@ -18,14 +21,50 @@ public record User
     public List<string> inventory { get; set; }= new List<string>();
     public int hp { get; set; }= 100;
     
-    
-    public class Equipment
+    public record Equipment
     {
-        public string helmet { get; set; }="";
-        public string armor { get; set; }= "";
-        public string pants { get; set; }= "";
-        public string shoes { get; set; }= "";
-        public string gloves { get; set; }="";
-        public string weapon { get; set; }="";
+        public string? helmet { get; set; } =null;
+        public string? armor { get; set; }= null;
+        public string? pants { get; set; }= null;
+        public string? shoes { get; set; }= null;
+        public string? gloves { get; set; }=null;
+        public string? weapon { get; set; }=null;
     }
+
+    public UserEquipment GetEquipmentAsItems(DataBase dataBase)
+    {
+        
+        UserEquipment res = new UserEquipment(
+            helmet_: equipment?.helmet == null ? null :(Item)dataBase.GetFromDataBase(Bases.Items, equipment.helmet)!,
+            armor_:  equipment?.armor == null ?  null :(Item)dataBase.GetFromDataBase(Bases.Items, equipment.armor)!,
+            pants_:  equipment?.pants == null ?  null :(Item)dataBase.GetFromDataBase(Bases.Items, equipment.pants)!,
+            shoes_:  equipment?.shoes == null ?  null :(Item)dataBase.GetFromDataBase(Bases.Items, equipment.shoes)!,
+            gloves_: equipment?.gloves == null ? null :(Item)dataBase.GetFromDataBase(Bases.Items, equipment.gloves)!,
+            weapon_: equipment?.weapon == null ? null :(Item)dataBase.GetFromDataBase(Bases.Items, equipment.weapon)!
+            );
+
+        return res;
+    }
+}
+
+
+public class UserEquipment
+{
+    public Item? helmet = null;
+    public Item? armor = null;
+    public Item? pants = null;
+    public Item? shoes = null;
+    public Item? gloves = null;
+    public Item? weapon = null;
+
+    public UserEquipment(Item? helmet_, Item? armor_, Item? pants_, Item? shoes_, Item? gloves_, Item? weapon_)
+    {
+        helmet = helmet_;
+        armor = armor_;
+        pants = pants_;
+        shoes = shoes_;
+        gloves = gloves_;
+        weapon = weapon_;
+    }
+    
 }

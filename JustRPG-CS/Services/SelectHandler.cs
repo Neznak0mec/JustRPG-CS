46a8c74c63905d57e191732ce9561e060c96692a
noname <smershicus@gmail.com>
2022-11-15 14:36:18 +0300

    create SelectHandler

---
+++ JustRPG-CS/Services/SelectHandler.cs
@@ -0,0 +1,38 @@
+using Discord.WebSocket;
+using JustRPG.Generators;
+using JustRPG.Modules.Responce;
+using Serilog;
+
+namespace JustRPG.Services;
+
+public class SelectHandler
+{
+    private SocketMessageComponent _component;
+    private InventoryInteractions _profileButtons;
+    
+    public SelectHandler(DiscordSocketClient client, SocketMessageComponent component, object service)
+    {
+        _component = component;
+        _profileButtons = new InventoryInteractions(client, component, service);
+    }
+    
+    public async Task SelectDistributor()
+    {
+        var buttonInfo = _component.Data.CustomId.Split('_');
+        if (_component.User.Id.ToString() != buttonInfo[1])
+        {
+            await WrongInteraction();
+            return;
+        }
+
+        if (buttonInfo[0].StartsWith("InvInteractionType"))
+            await _profileButtons.Distributor(buttonInfo);
+
+    }
+    
+    private async Task WrongInteraction()
+    {
+        await _component.RespondAsync(embed: EmbedCreater.ErrorEmbed("Вы не можете с этим взаимодействовать"));
+    }
+    
+}

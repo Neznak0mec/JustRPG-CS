a3b4193a1df4160e42e07f7f4fe25a28c12c20a1
Neznak0mec <smershicus@gmail.com>
2023-05-24 07:32:12 +0300

    Refactoring added dungeons and something else

---
+++ JustRPG-CS/Modules/Buttons/ActionInteractions.cs
@@ -0,0 +1,133 @@
+using Discord;
+using Discord.WebSocket;
+using JustRPG.Generators;
+using JustRPG.Interfaces;
+using JustRPG.Models;
+using JustRPG.Services;
+using Serilog;
+using Action = JustRPG.Models.Action;
+
+namespace JustRPG.Modules.Buttons;
+
+public class ActionInteractions : IInteractionMaster
+{
+    private DiscordSocketClient _client;
+    private SocketMessageComponent _component;
+    private readonly DataBase _dataBase;
+    private Action? _action;
+    private User _dbUser;
+
+    public ActionInteractions(DiscordSocketClient client, SocketMessageComponent component, object? service)
+    {
+        _client = client;
+        _component = component;
+        _dataBase = (DataBase)service!;
+    }
+
+    public async Task Distributor(string[] buttonInfo)
+    {
+        _action = (Action)_dataBase.ActionDb.Get($"Action_{buttonInfo[2]}");
+
+        if (_action == null)
+        {
+            await _component.UpdateAsync(x =>
+            {
+                x.Embed = EmbedCreater.ErrorEmbed("Неизвестная ошибка, попробуйте ещё раз");
+                x.Components = null;
+            });
+            return;
+        }
+
+        User _dbUser = (User)_dataBase.UserDb.Get(Convert.ToUInt64(buttonInfo[1]))!;
+        
+        if (buttonInfo[3] == "Denied")
+        {
+            await DeniedAction();
+            return;
+        }
+        
+        switch (_action.type)
+        {
+            case "Sell":
+                await AcceptSell();
+                break;
+            case "Equip":
+                await AcceptEquip();
+                break;
+        }
+    }
+
+    private async Task DeniedAction()
+    {
+        await _component.UpdateAsync(x =>
+        {
+            x.Embed = EmbedCreater.EmpEmbed("Действие было отменено");
+            x.Components = null;
+        });
+    }
+
+    private async Task AcceptSell()
+    {
+        Embed embed;
+        if (_dbUser.inventory.Contains(_action!.args[0]))
+        {
+            Item item = (Item)_dataBase.ItemDb.Get(_action.args[0])!;
+            int itemIndex = Array.IndexOf(_dbUser.inventory,item.id);
+            _dbUser.inventory = _dbUser.inventory.Where((x,y) => y != itemIndex).ToArray();
+            _dbUser.cash += item.price / 4;
+            
+            embed = EmbedCreater.SuccessEmbed($"Вы успешно продали `{item.name}` за `{item.price / 4}`");
+            _dataBase.UserDb.Update(_dbUser);
+        }
+        else
+        {
+            embed = EmbedCreater.ErrorEmbed("Данный предмет не найден в вашем инвентаре");
+        }
+        
+        await _component.UpdateAsync(x => {
+            x.Embed = embed;
+            x.Components = null;
+        });
+    }
+
+    private async Task AcceptEquip()
+    {
+        Embed embed;
+        if (_dbUser.inventory.Contains(_action!.args[0]))
+        {
+            Item item = (Item)_dataBase.ItemDb.Get(_action.args[0])!;
+            string? itemToChangeId = _dbUser.equipment!.GetByName(item.type);
+
+            if (itemToChangeId != null)
+            {
+                int indexInInventory = Array.IndexOf(_dbUser.inventory, item.id);
+                _dbUser.inventory[indexInInventory] = itemToChangeId;
+                _dbUser.equipment.SetByName(item.type,item.id);
+
+                var itemToChange = (Item)_dataBase.ItemDb.Get(itemToChangeId)!;
+
+                embed = EmbedCreater.SuccessEmbed($"Вы успешно сняли `{itemToChange.name}` и надели `{item.name}`");
+            }
+            else
+            {
+                int itemIndex = Array.IndexOf(_dbUser.inventory,item.id);
+                _dbUser.inventory = _dbUser.inventory.Where((x,y) => y != itemIndex).ToArray();
+                _dbUser.equipment.SetByName(item.type,item.id);
+                
+                embed = EmbedCreater.SuccessEmbed($"Вы успешно сняли надели `{item.name}`");
+            }
+            
+            _dataBase.UserDb.Update(_dbUser);
+        }
+        else
+        {
+            embed = EmbedCreater.ErrorEmbed("Данный предмет не найден в вашем инвентаре");
+        }
+        
+        await _component.UpdateAsync(x =>
+        {
+            x.Embed = embed;
+            x.Components = null;
+        });
+    }
+}
---
+++ JustRPG-CS/Modules/Buttons/ProfileInteractions.cs
@@ -0,0 +1,107 @@
+using Discord;
+using Discord.WebSocket;
+using JustRPG.Generators;
+using JustRPG.Interfaces;
+using JustRPG.Models;
+using JustRPG.Services;
+
+namespace JustRPG.Modules.Buttons;
+
+public class ProfileInteractions : IInteractionMaster
+{
+    private DiscordSocketClient _client;
+    private SocketMessageComponent _component;
+    private DataBase _dataBase;
+
+    public ProfileInteractions(DiscordSocketClient client, SocketMessageComponent component, object? service)
+    {
+        _client = client;
+        _component = component;
+        _dataBase = (DataBase)service!;
+    }
+    
+    public async Task Distributor(string[] buttonInfo) 
+    {
+        switch (buttonInfo[0])
+        {
+            case "Profile":
+                await ProfileButtonResponse(buttonInfo[2]);
+                break;
+            case "Equipment":
+                await EquipmentButtonResponse(buttonInfo[2]);
+                break;
+            case "Inventory":
+                await InventoryButtonResponse(buttonInfo[2]);
+                break;
+            case "UpSkills":
+                await UpSkillsButtonResponse(buttonInfo[1]);
+                break;
+            case "UpSkill":
+                await UpSkill(buttonInfo[1], buttonInfo[2]);
+                break;
+        }
+    }
+    
+    private async Task ProfileButtonResponse(string memberId)
+    {
+        var user = _client.GetUser(Convert.ToUInt64(memberId));
+        var userDb = (User) _dataBase.UserDb.Get(memberId)!;
+
+        await ResponceMessage(EmbedCreater.UserProfile(userDb, user),
+            ButtonSets.ProfileButtonsSet(_component.User.Id.ToString(), memberId));
+    }
+
+    private async Task EquipmentButtonResponse(string memberId)
+    {
+        var user = _client.GetUser(Convert.ToUInt64(memberId));
+        var userDb = (User)_dataBase.UserDb.Get( memberId)!;
+
+        await ResponceMessage(new EmbedCreater(_dataBase).UserEquipment(userDb, user),
+            ButtonSets.ProfileButtonsSet(_component.User.Id.ToString(), memberId, "Equipment"));
+    }
+
+    private async Task UpSkillsButtonResponse(string memberId)
+    {
+        var userDb = (User)_dataBase.UserDb.Get(memberId)!;
+        await ResponceMessage(EmbedCreater.UpSkills(), ButtonSets.UpUserSkills(memberId, userDb));
+    }
+
+    public async Task InventoryButtonResponse(string memberId)
+    {
+        var user = _client.GetUser(Convert.ToUInt64(memberId));
+
+        Inventory inventory = (Inventory)_dataBase.InventoryDb.Get( $"Inventory_{memberId}_{_component.User.Id.ToString()}")!;
+
+        var items = inventory.GetItems(_dataBase);
+        
+        await ResponceMessage(new EmbedCreater(_dataBase).UserInventory(user, items),
+            ButtonSets.InventoryButtonsSet(_component.User.Id.ToString(), Convert.ToInt64(memberId), inventory, items));
+
+    }
+
+    private async Task UpSkill(string memberId, string skill)
+    {
+        
+        var userDb = (User)_dataBase.UserDb.Get(memberId)!;
+        if (userDb.skillPoints <= 0)
+        {
+            await _component.RespondAsync( embed: EmbedCreater.ErrorEmbed("У вас недостаточно скилл поинтов"), ephemeral: true);
+            return;
+        }
+
+        _dataBase.UserDb.Add(userDb,skill,1);
+        _dataBase.UserDb.Add(userDb, "skill_points", -1);
+        userDb = (User)_dataBase.UserDb.Get(userDb.id)!;
+
+        await ResponceMessage(EmbedCreater.UpSkills(), ButtonSets.UpUserSkills(memberId, userDb));
+    }
+
+    private async Task ResponceMessage(Embed embed, MessageComponent component)
+    {
+        await _component.UpdateAsync(x =>
+        {
+            x.Embed = embed;
+            x.Components = component;
+        });
+    }
+}
